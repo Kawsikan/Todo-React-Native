17d59868746218775289260f4f2ef4649fb79089
kawsikan <kawsinote@gmail.com>
2021-07-19 22:42:27 +0530

    added bottom tab navigation

--- App.js
+++ App.js
@@ -5,6 +5,25 @@ import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-get-random-values';
 import Main from './components/Main';
 import Splash from './components/Splash';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 const Stack = createStackNavigator();
 
@@ -12,20 +31,26 @@ const App = () => {
 
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Splash"
-          component={Splash} 
-          options={{ title: 'Splash' }} />
-        <Stack.Screen
-          name="Main"
-          component={Main}
-          options={{ title: 'Welcome' }} />
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen
+    //       name="Splash"
+    //       component={Splash} 
+    //       options={{ title: 'Splash' }} />
+    //     <Stack.Screen
+    //       name="Main"
+    //       component={Main}
+    //       options={{ title: 'Welcome' }} />
 
-      </Stack.Navigator>
-    </NavigationContainer>
+    //   </Stack.Navigator>
+    // </NavigationContainer>
     // <Main />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Splash" component={Splash} />
+        <Tab.Screen name="Main" component={Main} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
