19a89e4b034a5cc3a398e5cf2c86f712acf3875a
kawsikan <kawsinote@gmail.com>
2021-07-20 23:09:34 +0530

    removed some

--- App.js
+++ App.js
@@ -7,21 +7,6 @@ import Main from './components/Main';
 import Splash from './components/Splash';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
